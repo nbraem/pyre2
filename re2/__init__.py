90a7468455bce6d01f3130a7fb6f9d763618571e
Nicolas Braem <nicolas.braem@gmail.com>
2015-01-30 01:26:33 -0800

    adblockparser wrapper

--- re2/__init__.py
+++ re2/__init__.py
@@ -1,1 +1,25 @@
-from re2._re2 import *
+import re2._re2
+
+def _to_bytes(s):
+  return bytes(s, 'utf-8')
+
+def search(pattern, in_string, flags=0):
+  return re2._re2.search(_to_bytes(pattern), _to_bytes(in_string), flags)
+
+def match(pattern, in_string, flags=0):
+  return re2._re2.match(_to_bytes(pattern), _to_bytes(in_string), flags)
+
+def finditer(pattern, in_string, flags):
+  return re2._re2.finditer(_to_bytes(pattern), _to_bytes(in_string), flags)
+
+def findall(pattern, in_string, flags=0):
+  return re2._re2.findfall(_to_bytes(pattern), _to_bytes(in_string), flags)
+
+class Pattern:
+  def __init__(self, p):
+    self._p = p
+
+  def search(self, in_string, pos=0, endpos=-1):
+    return self._p.search(_to_bytes(in_string), pos, endpos)
+
+  return Pattern(re2._re2.compile(_to_bytes(pattern), flags, max_mem))
